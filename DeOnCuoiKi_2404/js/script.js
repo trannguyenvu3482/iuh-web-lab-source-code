7a1c8c9fe3d1ae19cfe39176804efab17eaac4a4
trannguyenvu3482 <21088201.vu@student.iuh.edu.vn>
2023-04-24 16:46:21 +0700

    added DeOnCuoiKi_2404

---
+++ DeOnCuoiKi_2404/js/script.js
@@ -0,0 +1,52 @@
+const txtMa = document.querySelector('.txt-ma');
+const errMa = document.querySelector('.errorMa');
+const txtTen = document.querySelector('.txt-ten');
+const txtNgay = document.querySelector('.txt-ngay');
+const selectLoai = document.querySelector('.select-loai');
+const btnNhapKho = document.querySelector('.btn-nhapkho');
+
+const checkMaSP = (maSP) => {
+  const regMa = /^(SP)\d{4}(NK|XK)$/;
+  if (regMa.test(maSP)) {
+    errMa.textContent = '*';
+    txtMa.classList.remove('border-text');
+    errMa.classList.remove('text-danger');
+    return true;
+  } else {
+    errMa.textContent = 'Ma sai cu phap';
+    errMa.classList.add('text-danger');
+    txtMa.classList.add('border-text');
+  }
+};
+
+btnNhapKho.addEventListener('click', () => {
+  const maSP = txtMa.value;
+  const tenSP = txtTen.value;
+  const ngayNhap = txtNgay.value;
+  const loaiSP = selectLoai.value;
+  const thue = '10%';
+  const thanhToan = document.querySelector('.rad-btns:checked').value;
+  let loaiHangHoa = [];
+  document
+    .querySelectorAll('.chk-loaihanghoa:checked')
+    .forEach((item) => loaiHangHoa.push(item.value));
+
+  if (checkMaSP(maSP)) {
+    const html = `
+     <tr>
+      <td>${maSP}</td>
+      <td>${tenSP}</td>
+      <td>${ngayNhap}</td>
+      <td>${loaiSP}</td>
+      <td>${loaiHangHoa.join(',')}</td>
+      <td>${thanhToan}</td>
+      <td>${thue}</td>
+    </tr>
+    
+    `;
+
+    document.querySelector('table tbody').insertAdjacentHTML('beforeend', html);
+
+    document.querySelector('.btn-close').click();
+  }
+});
